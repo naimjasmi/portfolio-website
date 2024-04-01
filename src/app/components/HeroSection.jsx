945d6e9047f9c99bce21d340541d6e0db790b2f7
Your Name <you@example.com>
2024-04-01 13:29:26 +0800

    create a hero section and add picture to the website

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <div className="grid grid-cols-1 lg:grid-cols-12">
+                <div className="col-span-7 place-self-center">
+                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+                        Hero Section
+                    </h1>
+                    <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    </p>
+                    <div>
+                        <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
+                        <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-5">Download CV</button>
+                    </div>
+                </div>
+                <div className="col-span-5 place-self-center mt-4 lg:mt-8">
+                    <div className="rounded-full bg-[#181818] w-[350px] h-[250px] lg:w-[500px] lg:h-[400px] relative">
+                        <Image
+                            src="/images/sapiens.png"
+                            alt="hero image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection;
