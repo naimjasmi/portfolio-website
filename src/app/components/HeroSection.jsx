1c51af997061026f13056db319f50f65c03f4626
Your Name <you@example.com>
2024-04-02 13:44:10 +0800

    create a navbar, and pop up menu for mobile

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,9 +44,9 @@ const HeroSection = () => {
                     </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mt-8">
-                    <div className="rounded-full bg-[#181818] w-[350px] h-[250px] lg:w-[500px] lg:h-[400px] relative">
+                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
-                            src="/images/sapiens.png"
+                            src="/images/naimanimate.png"
                             alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             width={300}
