65175a9e01d65c131ae7fc20a2ab9162325b2c00
Your Name <you@example.com>
2024-04-08 09:34:54 +0800

    add button for project category

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./projectTag";
 
 const ProjectData = [
     {
@@ -8,6 +10,8 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/1.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
     },
     {
         id: 2,
@@ -15,6 +19,9 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/2.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+
     },
     {
         id: 3,
@@ -22,6 +29,9 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/3.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+
     },
     {
         id: 4,
@@ -29,6 +39,9 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/4.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+
     },
     {
         id: 5,
@@ -36,6 +49,9 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/5.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+
     },
     {
         id: 6,
@@ -43,25 +59,52 @@ const ProjectData = [
         description: "Project description",
         image: "/images/projects/6.jpg",
         tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+
     },
 
 ]
 const ProjectSection = () => {
+    const [tag, setTag] = useState("All");
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+
     return (
         <>
             <h2 className="text-center text-4xl font-bold text-white mt-4">
-            My Projects
+                My Projects
             </h2>
-            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-            {ProjectData.map((project) =>
-                <ProjectCard
-                    key={project.id}
-                    title={project.title}
-                    description={project.description}
-                    imgUrl={project.image}
-                    tags={project}
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="All"
+                    isSelected={tag === "All"}
                 />
-            )}
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Web"
+                    isSelected={tag === "Web"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Mobile"
+                    isSelected={tag === "Mobile"}
+                />
+            </div>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {ProjectData.map((project) =>
+                    <ProjectCard
+                        key={project.id}
+                        title={project.title}
+                        description={project.description}
+                        imgUrl={project.image}
+                        gitUrl={project.gitUrl}
+                        previewUrl={project.previewUrl}
+                    />
+                )}
             </div>
         </>
 
