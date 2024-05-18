e557ea3af9f8dd8805b4606324690e47f30a6ee3
Your Name <you@example.com>
2024-05-18 19:01:49 +0800

    rollback

--- src/app/components/ResumeModal.jsx
+++ src/app/components/ResumeModal.jsx
@@ -1,25 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 
-const ResumeModal = ({ isOpen, onRequestClose, resumeUrl }) => {
-  useEffect(() => {
-    Modal.setAppElement('#__next');
-  }, []);
+Modal.setAppElement('#__next');
 
+const ResumeModal = ({ isOpen, onRequestClose, resumeUrl }) => {
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className="fixed inset-0 flex flex-col items-center justify-center p-4 bg-black bg-opacity-75"
+      className="fixed inset-0 flex items-center justify-center p-4 bg-black bg-opacity-75"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50"
     >
-      <div className="bg-white p-6 rounded-lg max-w-3xl mx-auto w-full relative">
-        <button
-          onClick={onRequestClose}
-          className="absolute top-4 right-4 text-xl text-gray-500 hover:text-gray-700 cursor-pointer"
-        >
-          ✕ {/* Unicode character for the close (X) symbol */}
-        </button>
+      <div className="bg-white p-6 rounded-lg max-w-3xl mx-auto w-full">
         <h2 className="text-2xl mb-4">My Resume</h2>
         <iframe
           src={resumeUrl}
@@ -27,6 +19,12 @@ const ResumeModal = ({ isOpen, onRequestClose, resumeUrl }) => {
           height="600px"
           className="border-none"
         />
+        <button
+          onClick={onRequestClose}
+          className="mt-4 px-4 py-2 bg-gradient-to-br from-primary-500 to-secondary-500 text-white rounded"
+        >
+          Close
+        </button>
       </div>
     </Modal>
   );
