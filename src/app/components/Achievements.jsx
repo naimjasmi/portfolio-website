05c4a9f5e3f5c7e001379698fccd6fd1c5557a0e
Your Name <you@example.com>
2024-05-18 14:56:34 +0800

    fix margin and allignment

--- src/app/components/Achievements.jsx
+++ src/app/components/Achievements.jsx
@@ -1,61 +1,67 @@
-"use client"
+"use client";
 import React from "react";
 import dynamic from "next/dynamic";
 
-const AnimatedNumbers = dynamic(() => { return import("react-animated-numbers") }, { ssr: false })
-const AchievementSection = () => {
+const AnimatedNumbers = dynamic(
+  () => {
+    return import("react-animated-numbers");
+  },
+  { ssr: false }
+);
 
-    const achievementList = [
-        {
-            metric: "Projects",
-            value: "3",
-        },
-        {
-            metric: "Users",
-            value: "1",
-        },
-        {
-            metric: "Awards",
-            value: "1",
-        },
-        {
-            metric: "Years",
-            value: "5",
-        },
-    ]
-    return (
-        <div className="py-8 px-4 xl:gap-16 smp:py-16 xl:px-16">
-            <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-                {achievementList.map((achievement, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="flex flex-col items-center justify-center mx-4">
-                            <h2 className="text-white text-4xl font-bold flex flex-row">
-                                <AnimatedNumbers
-                                    includeComma
-                                    animateToNumber={parseInt(achievement.value)}
-                                    locale="en-US"
-                                    className="text-white text-4xl font-bold"
-                                    configs={(_, index) => {
-                                        return {
-                                            mass: 1,
-                                            friection: 100,
-                                            tesnion: 140 * (index + 1),
-                                        };
-                                    }}
-                                />
-                            </h2>
-                            <p className="text-[#ADB7BE] text-base">
-                                {achievement.metric}
-                            </p>
-                        </div>
-                    );
-                })
-                }
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: "2",
+  },
+  {
+    prefix: "~",
+    metric: "Users",
+    value: "3",
+  },
+  {
+    metric: "Awards",
+    value: "1",
+  },
+  {
+    metric: "Years",
+    value: "5",
+  },
+];
+
+const AchievementsSection = () => {
+  return (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold flex flex-row">
+                {achievement.prefix}
+                <AnimatedNumbers
+                  includeComma
+                  animateToNumber={parseInt(achievement.value)}
+                  locale="en-US"
+                  className="text-white text-4xl font-bold"
+                  configs={(_, index) => {
+                    return {
+                      mass: 1,
+                      friction: 100,
+                      tensions: 140 * (index + 1),
+                    };
+                  }}
+                />
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default AchievementSection
+export default AchievementsSection;
